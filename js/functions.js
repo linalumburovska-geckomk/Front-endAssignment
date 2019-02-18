3a85460f4fcba34b63dd77aaab65b83d4e53a2ff
linalumburovska <linalumburovska20@gmail.com>
2019-02-18 16:01:18 +0100

    Validation in step 2 added, changed js file with class and ajax

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,149 @@
-//Step1- Button "Create feed" on click loads step 2
-$("#button1").click(function(e){
-    e.preventDefault();
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("step1").innerHTML = this.responseText;
+var App = function() {
+    this.pageMap = {}
+    this.currentPage = null
+    this.data = {}
+}
+
+App.prototype.addPage = function(url, page) {
+    if (this.isPageValid(page)) {
+        this.pageMap[url] = page
+    }
+}
+
+App.prototype.forward = function(url) {
+    if (typeof this.pageMap[url] !== 'undefined') {
+        if (this.currentPage !== null) {
+            this.currentPage.dispose()
         }
-    };
-    xhttp.open("GET", "step2.html", true);
-    xhttp.send();
-});
+        this.currentPage = this.pageMap[url]
+        var self = this
+        this.currentPage.load().then(function() {
+            self.currentPage.init()
+        }).catch(function(e) {
+            console.error(e)
+        })
+        // Implement the history api from the browser
+    }
+}
+
+App.prototype.isPageValid = function(page) {
+    var loadState = typeof page['load'] === 'function'
+    var initState = typeof page['init'] === 'function'
+    var disposeState = typeof page['dispose'] === 'function'
+    return loadState && initState && disposeState
+}
+
+var Step1Page = function(app) {
+    this.app = app
+}
+
+Step1Page.prototype.load = function() {
+    return new Promise(function(resolve) {
+        $.ajax({
+            async: true,
+            url: "step1.html",
+            type: 'GET',
+            success: function(data) {
+                $('#root').empty()
+                $('#root').append(data)
+                resolve()
+            }
+        });
+    })
+}
+
+Step1Page.prototype.init = function() {
+    console.log(this.app.data['haha'])
+    var self = this
+    $('#button1').on('click', function(e) {
+        e.preventDefault()
+        self.app.forward('step2')
+    })
+}
+
+Step1Page.prototype.dispose = function() {
+    console.log('dispose')
+}
+
+var Step2Page = function(app) {
+    this.app = app
+}
+
+Step2Page.prototype.load = function() {
+    return new Promise(function(resolve) {
+        $.ajax({
+            async: true,
+            url: "step2.html",
+            type: 'GET',
+            success: function(data) {
+                $('#root').empty()
+                $('#root').append(data)
+                resolve()
+            }
+        });
+    })
+}
+
+Step2Page.prototype.init = function() {
+    var self = this
+    $('#back').on('click', function() {
+        self.app.forward('step1')
+    })
+    $('#save').on('click', function() {
+        if(($('#name').val()!='') &&  ($('#description').val()!='') &&  ($('#imageURL').val()!='')){
+            self.app.forward('step3')
+        } else if($('#name').val() ==""){
+            $('#errorName').show()
+        } else if($('#description').val()=="") {
+            $('#errorName').hide()
+            $('#errorDescription').show()
+        } else if($('#imageURL').val()==""){
+            $('#errorName').hide()
+            $('#errorDescription').hide()
+            $('#errorImage').show()
+        }
+    })
+}
+
+Step2Page.prototype.dispose = function() {
+    this.app.data['name'] = $('#name').val()
+    this.app.data['description'] = $('#description').val()
+    this.app.data['imageurl'] = $('#imageURL').val()
+    console.log('dispose')
+}
+
+var Step3Page = function(app) {
+    this.app = app
+}
+
+Step3Page.prototype.load = function() {
+    return new Promise(function(resolve) {
+        $.ajax({
+            async: true,
+            url: "step3.html",
+            type: 'GET',
+            success: function(data) {
+                $('#root').empty()
+                $('#root').append(data)
+                resolve()
+            }
+        });
+    })
+}
+
+Step3Page.prototype.init = function() {
+    $('#nameStep3').append(this.app.data['name'])
+
+}
+
+Step3Page.prototype.dispose = function() {
+    console.log('dispose')
+}
+
+$('window').ready(function() {
+    var app = new App()
+    app.addPage('step1', new Step1Page(app))
+    app.addPage('step2', new Step2Page(app))
+    app.addPage('step3', new Step3Page(app))
+    app.forward('step1')
+})
