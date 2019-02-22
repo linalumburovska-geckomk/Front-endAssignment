777d2c0b0832ff1984dd49c7f7c1bdc3ca5b2090
linalumburovska <linalumburovska20@gmail.com>
2019-02-22 14:17:48 +0100

    Sanitized form from step2, secure input

--- js/pages/step2.js
+++ js/pages/step2.js
@@ -3,8 +3,12 @@
     define (function(require){
     
         var $ = require('jquery')
-        
-    
+        require('bootstrap')
+
+        var name=""
+        var desc=""
+        var image=""
+
         var Step2Page = function(app) {
             this.app = app
         }
@@ -30,49 +34,105 @@
                 self.app.forward('step1')
             })
             $('#save').on('click', function() {
-                var name = $('#name').val()
-                var desc= $('#description').val()
-                var image = $('#imageURL').val()
-                if(name!='' &&  desc!='' &&  image!=''){
-                    self.app.forward('step3')
-                } else if(name!='' &&  desc=='' &&  image=='') {
-                    $('#errorName').hide()
-                    $('#errorDescription').show()
-                    $('#errorImage').show()
-                } else if(name=='' &&  desc!='' &&  image=='') {
-                    $('#errorName').show()
-                    $('#errorDescription').hide()
-                    $('#errorImage').show()
-                } else if(name=='' &&  desc=='' &&  image!='') {
-                    $('#errorName').show()
-                    $('#errorDescription').show()
-                    $('#errorImage').hide()
-                } else if(name!='' &&  desc!='' &&  image=='') {
-                    $('#errorName').hide()
-                    $('#errorDescription').hide()
-                    $('#errorImage').show()
-                } else if(name!='' &&  desc=='' &&  image!='') {
-                    $('#errorName').hide()
-                    $('#errorDescription').show()
-                    $('#errorImage').hide()
-                } else if(name=='' &&  desc!='' &&  image!='') {
-                    $('#errorName').show()
-                    $('#errorDescription').hide()
-                    $('#errorImage').hide()
+                name = $('#name').val()
+                desc= $('#description').val()
+                image = $('#imageURL').val()
+
+                // Secure input, checks if all input fields are valid
+                if($.sanitize(name)==-1) {
+                    $('#sanitizeName').show()    
+                    $('#sanitizeDescription').hide()
+                    $('#sanitizeImage').hide()               
+                } else if($.sanitize(desc)==-1) {
+                    $('#sanitizeName').hide()  
+                    $('#sanitizeDescription').show()  
+                    $('#sanitizeImage').hide()    
+                } else if($.sanitize(image)==-1) {
+                    $('#sanitizeName').hide()  
+                    $('#sanitizeDescription').hide()
+                    $('#sanitizeImage').show()
                 } else {
-                    $('#errorName').show()
-                    $('#errorDescription').show()
-                    $('#errorImage').show()
+                    //If they are secure, checks if every field that is requred is not empty
+                    name=$.sanitize(name)
+                    desc=$.sanitize(desc)
+                    image=$.sanitize(image)
+
+                    if(name!='' &&  desc!='' &&  image!=''){
+                        self.app.forward('step3')
+                    } else if(name!='' &&  desc=='' &&  image=='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').hide()
+                        $('#errorDescription').show()
+                        $('#errorImage').show()
+                    } else if(name=='' &&  desc!='' &&  image=='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').show()
+                        $('#errorDescription').hide()
+                        $('#errorImage').show()
+                    } else if(name=='' &&  desc=='' &&  image!='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').show()
+                        $('#errorDescription').show()
+                        $('#errorImage').hide()
+                    } else if(name!='' &&  desc!='' &&  image=='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').hide()
+                        $('#errorDescription').hide()
+                        $('#errorImage').show()
+                    } else if(name!='' &&  desc=='' &&  image!='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').hide()
+                        $('#errorDescription').show()
+                        $('#errorImage').hide()
+                    } else if(name=='' &&  desc!='' &&  image!='') {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').show()
+                        $('#errorDescription').hide()
+                        $('#errorImage').hide()
+                    } else {
+                        $('#sanitizeName').hide()
+                        $('#sanitizeDescription').hide()
+                        $('#sanitizeImage').hide()
+                        $('#errorName').show()
+                        $('#errorDescription').show()
+                        $('#errorImage').show()
+                    }
                 }
+                               
             })
         }
         
         
         Step2Page.prototype.dispose = function() {
-            this.app.data['name'] = $('#name').val().toUpperCase()
-            this.app.data['description'] = $('#description').val()
-            this.app.data['imageurl'] = $('#imageURL').val()
+            this.app.data['name'] = name.toUpperCase()
+            this.app.data['description'] = desc
+            this.app.data['imageurl'] = image
         }
+
+
+        $.sanitize = function(input) {
+            var output = input.replace(/<script[^>]*?>.*?<\/script>/gi, '').
+                         replace(/<[\/\!]*?[^<>]*?>/gi, '').
+                         replace(/<style[^>]*?>.*?<\/style>/gi, '').
+                         replace(/<![\s\S]*?--[ \t\n\r]*>/gi, '');
+            if(input==output){
+                return output;
+            } else {
+                return -1;
+            }
+        };
     
         return Step2Page;
     });
